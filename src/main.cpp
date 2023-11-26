48e3c3892df6cac9d134e7e42cf3e214de7a118c
gavinswilson@gmail.com <gavinswilson@gmail.com>
2023-11-25 20:35:52 -0800

    added div measure

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,15 @@
 #include <string>
 #include <cmath>
 
+
+int divisible_by_num(int num, int div)
+{
+    if (num % div == 0)
+        return 1;
+    else
+        return 0;
+}
+
 int main(int argc, char* argv[]) 
 { 
     std::cout << "You have entered " << argc << " arguments" << std::endl;
@@ -11,6 +20,11 @@ int main(int argc, char* argv[])
     for (int i = 1; i < argc; i++) 
     {
         std::cout << argv[i] << " " << std::endl;
+
+        int answer = divisible_by_num(std::stoi(argv[i]),11);
+
+        std::cout << std::stoi(argv[i]) << " " << answer << std::endl;
+
     }
     
     
